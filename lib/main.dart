8d0bc7d4e38a934a4e5f4f1be0d7cd3fbd9e179a
dhruvichaudhary76-source <dhruvichaudhary76@gmail.com>
2026-09-19 23:31:05 +0530

    Refactor MyApp to use StatefulWidget and WebViewController

--- lib/main.dart
+++ lib/main.dart
@@ -2,32 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..enableZoom(false)
+      ..clearCache()
+      ..clearLocalStorage()
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onNavigationRequest: (request) {
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://dsapalwal.in/'));
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: SafeArea(
-          child: WebViewWidget(
-            controller: WebViewController()
-              ..setJavaScriptMode(JavaScriptMode.unrestricted)
-              ..setBackgroundColor(const Color(0x00000000))
-              ..setNavigationDelegate(
-                NavigationDelegate(
-                  onPageStarted: (url) {},
-                  onPageFinished: (url) {},
-                  onWebResourceError: (error) {},
-                ),
-              )
-              ..loadRequest(Uri.parse('https://dsapalwal.in')),
-          ),
-        ),
+        body: SafeArea(child: WebViewWidget(controller: controller)),
       ),
     );
   }
